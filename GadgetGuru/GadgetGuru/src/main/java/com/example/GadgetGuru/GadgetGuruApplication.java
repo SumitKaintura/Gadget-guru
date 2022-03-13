7a6320ccd58d08b5b5167bbdee3f1bab20cfbfe6
SumitKaintura <43599009+SumitKaintura@users.noreply.github.com>
2022-03-13 23:34:39 +0530

    added

---
+++ GadgetGuru/GadgetGuru/src/main/java/com/example/GadgetGuru/GadgetGuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.GadgetGuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetGuruApplication.class, args);
+	}
+
+}
---
+++ GadgetGuru/GadgetGuru/src/test/java/com/example/GadgetGuru/GadgetGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GadgetGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
